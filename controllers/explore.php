0d758d4083e85236bf978ec49e5ed51f3e52e36b
Brendan <brendan.metzger@gmail.com>
2015-03-18 17:04:30 -0500

    modifying objects to be iterated instead of constructed

--- controllers/explore.php
+++ controllers/explore.php
@@ -13,9 +13,16 @@ class explore
     $view = new view('visible/layout.html');
     $view->content = 'visible/home.html';
 		
+    $data = new \stdClass;
+    $data->supporters = [
+      ['name' => 'The MacArthur Foundation'],
+      ['name' => 'The Richard H. Driehaus Foundation'],
+      ['name' => 'The Boeing Company'],
+      ['name' => 'Individual Donors']
+    ];
     
     
-    // $plat = new view\plat($view->xpath->query('//body//article')->item(0));
+    $plat = new view\plat($view, $data);
 
     
     print $view->render();
