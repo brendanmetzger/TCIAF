6d0f4ed40c9cf7919e690d5d604aea000fcce3ae
Brendan <brendan.metzger@gmail.com>
2015-03-25 10:32:41 -0500

    updating submodule

--- controllers/superintend.php
+++ controllers/superintend.php
@@ -68,7 +68,7 @@ class superintend
     
     $this->features = $db->query("SELECT * FROM features LIMIT 25")->fetch_all(MYSQLI_ASSOC);
     $view->content = 'views/feature.html';
-    
+    // \bloc\application::dump($this->registry);
     // $fragment = $view->dom->createDocumentFragment();
     // $fragment->appendXML("<ul><li>[@origin_country]</li><li>[@premier_locaction]</li><li>[@premier_date]</li><li>[@published]</li><li>[@delta]</li></ul>");
     // \bloc\application::dump(new view($fragment));
