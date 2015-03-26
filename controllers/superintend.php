bfeaee53e8b8b3165140d2cc594882bae421003c
Brendan <brendan.metzger@gmail.com>
2015-03-25 23:00:37 -0500

    organizing. controllers are starting to take shape

--- controllers/superintend.php
+++ controllers/superintend.php
@@ -13,23 +13,20 @@ class superintend extends \bloc\controller
     'layout' => 'views/layout.html',
   ];
   
-      
   public function __construct($request, $access)
   {
     View::addRenderer('before', view\renderer::addPartials($this));
     View::addRenderer('after', view\renderer::HTML());
-    
+
     $this->authenticated = (isset($_SESSION) && array_key_exists('user', $_SESSION));
-		$this->year  = date('Y');
+		$this->year = date('Y');
     $this->title = "Third Coast";
-    
     $this->supporters = [
       ['name' => 'The MacArthur Foundation'],
       ['name' => 'The Richard H. Driehaus Foundation'],
       ['name' => 'The Boeing Company'],
       ['name' => 'Individual Donors']
     ];
-
     
     if ($this->authenticated) {
       $this->partials['helper'] = 'views/admin.html';
@@ -39,36 +36,27 @@ class superintend extends \bloc\controller
   
   public function index()
   {
-    echo 'hi';
+    print (new View($this->partials['layout']))->render($this());
   }
   
   public function login($redirect_url, $post_data)
   {
-    $view = new View($this->partials['layout']);
-    
+    $view = new View($this->partials['layout']);    
     $view->content = 'views/forms/credentials.html';
     
-    $data = new \bloc\types\dictionary;
+    $username = array_key_exists('username', $post_data) ? $post_data['username'] : '';
+    $password = array_key_exists('password', $post_data) ? $post_data['password'] : '';
     
-    $data->username = array_key_exists('username', $post_data) ? $post_data['username'] : '';
-    $data->password = array_key_exists('password', $post_data) ? $post_data['password'] : '';
-    
-    $users = new \bloc\DOM\Document('data/users', ['validateOnParse' => true]);
-    $user = $users->getElementById($data->username);
-    if ($user && password_verify($data->password, $user->getAttribute('password'))) {
+    if ($user = (new \models\person)->authenticate($username, $password)) {
       session_start();
-      $_SESSION['user'] = $data->username;
-      header("Location: {$redirect_url}");
-      exit();
+      $_SESSION['user'] = $user->getAttribute('username');
+      \bloc\router::redirect('/');
     }
     
-    $data->year = 2015;
-    $data->action = $redirect_url;
-    $data->title = 'TCIAF';
-    $data->password = null;   
-    print $view->render($data);
-    
-  }
-  
+    $this->action   = $redirect_url;
+    $this->username = $username;
+    $this->password = null;   
 
+    print $view->render($this());
+  }
 }
--- models/person.php
+++ models/person.php
@@ -7,4 +7,20 @@ namespace models;
 
 class Person
 {
+  private $storage;
+  
+  public function __construct()
+  {
+    $this->storage = new \bloc\DOM\Document('data/users', ['validateOnParse' => true]);
+  }
+  
+  public function authenticate($username, $password)
+  {
+    if ($user = $this->storage->getElementById($username)) {
+      if (password_verify($password, $user->getAttribute('hash'))) {
+        return $user;
+      }
+    }
+    return false;
+  }
 }
