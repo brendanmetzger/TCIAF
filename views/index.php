0ba806492c912c1e5a4de1295ec5f7ec2ba7275a
Brendan <brendan.metzger@gmail.com>
2015-11-19 16:39:26 -0600

    notice in index

--- views/index.php
+++ views/index.php
@@ -24,17 +24,17 @@ $app->prepare('http-request', function ($app, $params) {
   $response = new Response($request);
 
   $app->setExchanges($request, $response);
-  
+
   // Provide a namespace (also a directory) to load objects that can respond to controller->action
   $router  = new Router('controllers', $request);
-  
+
   // default controller and action as arguments, in case nothin doin in the request
   $response->setBody($router->delegate('explore', 'index'));
-  
+
 
   $app->execute('debug', $response);
 
-  
+
   echo $response;
 });
 
@@ -46,13 +46,14 @@ $app->prepare('clean-up', function ($app) {
 
 
 $app->prepare('debug', function ($app, $response) {
+
   if (getenv('MODE') === 'local') {
     $app::instance()->log('Peak Memory: ' . round(memory_get_peak_usage() / pow(1024, 2), 4). "Mb");
     $app::instance()->log('Executed in: ' . round(microtime(true) - $app->benchmark, 4) . "s ");
-    
+
     $output = $response->getBody();
     if ($output instanceof \bloc\view) {
-      
+
       $elem = (new DOM\Element('script'))->insert($output->dom->documentElement->lastChild);
       $elem->setAttribute('type', 'text/javascript');
       $elem->appendChild($elem->ownerDocument->createTextNode("console.group('Backend notes');"));
@@ -60,12 +61,14 @@ $app->prepare('debug', function ($app, $response) {
         $elem->appendChild($elem->ownerDocument->createTextNode(sprintf("console.log(%s);", json_encode($message))));
       }
       $elem->appendChild($elem->ownerDocument->createTextNode("console.groupEnd();"));
-    
+
     } else if ($response->type == 'html'){
       $response->setBody($output . "<pre>" . print_r($app::instance()->log(), true) . "</pre>");
     }
+  } else {
+    return '';
   }
-  
+
   return $output;
 });
 
