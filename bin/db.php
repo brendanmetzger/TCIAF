a8aae069fa304e19ddc0413b0fad4e749129af6d
Brendan <brendan.metzger@gmail.com>
2016-05-19 10:11:13 -0500

    adding link, making  sure images behave

--- bin/db.php
+++ bin/db.php
@@ -6,11 +6,19 @@ print "Server Listening on $addr:$port\n";
 while($c = socket_accept($sock)) {
    /* do something useful */
    socket_getpeername($c, $raddr, $rport);
-   for ($i=0; $i < 10; $i++) {
-     sleep(1);
-     socket_write($c, "Hello, {$i}");
+   socket_write($c, "what do you want?\n");
+
+   if ($query = trim(socket_read($c, 1024, PHP_NORMAL_READ))) {
+
+     for ($i=0; $i < 10; $i++) {
+       $msg = "{$query}, {$i}\n";
+       print $msg;
+       socket_write($c, $msg);
+       sleep(1);
+     }
+     socket_close($c);
    }
-   socket_close($c);
+
    print "Received Connection from $raddr:$rport\n";
 }
 socket_close($sock);
