b10b89dd5490b48e057e5166021edcab9160404b
Brendan <brendan.metzger@gmail.com>
2016-05-19 09:41:59 -0500

    about page issue

--- bin/db.php
+++ bin/db.php
@@ -2,12 +2,15 @@
 $sock = socket_create_listen(0);
 socket_getsockname($sock, $addr, $port);
 print "Server Listening on $addr:$port\n";
-$fp = fopen('/tmp/sock-play.txt', 'w');
-fwrite($fp, $port);
-fclose($fp);
+
 while($c = socket_accept($sock)) {
    /* do something useful */
    socket_getpeername($c, $raddr, $rport);
+   for ($i=0; $i < 10; $i++) {
+     sleep(1);
+     socket_write($c, "Hello, {$i}");
+   }
+   socket_close($c);
    print "Received Connection from $raddr:$rport\n";
 }
 socket_close($sock);
