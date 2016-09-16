73911e5686218d022e10070858771fb35e549c25
Brendan Metzger <brendan.metzger@gmail.com>
2016-09-16 16:12:09 -0500

    updating backup

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,5 @@
 # Git: add and commit changes
-cd /var/www/thirdcoastfestival.org/data && /usr/bin/git commit -a -m "data push `date`"
+cd /var/www/thirdcoastfestival.org/data && /usr/bin/git add --all && /usr/bin/git commit -m "data push `date`"
 
 # send data to Git server
 cd /var/www/thirdcoastfestival.org/data && /usr/bin/git push origin master
