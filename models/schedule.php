32062057433032b6467836826ddedb7e6f2762f9
Brendan <brendan.metzger@gmail.com>
2019-10-03 11:21:07 -0500

    computing schedule ranges and offsets

--- models/schedule.php
+++ models/schedule.php
@@ -2,16 +2,22 @@
 
 
 class Schedule {
+  private $size;
   private $dataset = [
     'events' => [],
     'range'  => [], // in hours
   ];
   
   public function __construct (\iterator $edges) {
-    
+    $this->size = $edges->count();
     foreach ($edges as $edge) {
       $this->place($edge);
     }
+    usort($this->dataset['events'], function ($a, $b) {
+      return $a['item']['start'] > $b['item']['start'];
+    });
+    
+    \bloc\application::instance()->log($this->draw());
   }
   
   public function render() {
@@ -19,11 +25,22 @@ class Schedule {
     return $this->dataset['events'];
   }
   
-  public function getRange($value='') {
-    # code...
+  // divisor converts the length in seconds to something else (default is days 60 * 60 * 24)
+  public function draw() {
+    $first = $this->dataset['events'][0]['item']['start'];
+    $last = $this->dataset['events'][$this->size - 1]['item']['end'];
+    $start_offset = mktime(0, 0, 0, ...explode(',', date('m,d,y', $first)));
+    $total_duration = ($last - $first) / 60;
+    foreach ($this->dataset['events'] as &$event) {
+      $event['item']['offset'] = (($event['item']['start'] - $start_offset) / 60) / $total_duration;
+      $event['item']['size'] = $event['item']['duration'] / $total_duration;
+    }
+    
+    
+    return $this->dataset['events'];
   }
   
-  public function groupToDays($value='') {
+  public function groupToDays() {
     # code...
   }
   
@@ -42,7 +59,6 @@ class Schedule {
     
     $event['duration'] = ($event['end'] - $event['start']) / 60;
     
-    \bloc\application::instance()->log($event);
     $this->dataset['events'][] = ['item' => $event];
   }
 }
